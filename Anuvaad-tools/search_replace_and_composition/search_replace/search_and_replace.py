029415786036df0f3b5448027e2638908b1550e2
Mayank Morya <moryamayank9@gmail.com>
2020-03-06 22:58:27 +0530

    temp change removed

--- Anuvaad-tools/search_replace_and_composition/search_replace/search_and_replace.py
+++ Anuvaad-tools/search_replace_and_composition/search_replace/search_and_replace.py
@@ -83,8 +83,8 @@ def process(search_replaces, processId, workspace, config, file, file_count):
     log.info('process : started at ' + str(start_time))
     lines = readfile(processId, file)
     total_sentences = get_sentences_count(lines)
-    if total_sentences > 255000:
-        raise Exception('more than 250000 lines')
+    if total_sentences > 275000:
+        raise Exception('more than 275000 lines')
     line_count = 1
     sentence_matched = 0
     not_matched = list()
